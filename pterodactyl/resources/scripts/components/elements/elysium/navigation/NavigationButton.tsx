fa12bef6d268a2aaaab19980005ae42ca81292c8
Muhammad Zaki <162775830+jakisoft@users.noreply.github.com>
2026-04-17 12:56:15 +0700

    Upload pterodactyl/resources/scripts/components/elements/elysium/navigation/NavigationButton.tsx

---
+++ pterodactyl/resources/scripts/components/elements/elysium/navigation/NavigationButton.tsx
@@ -0,0 +1,12 @@
+import tw from "twin.macro";
+import styled from "styled-components/macro";
+
+const NavigationButton = styled.button`
+  ${tw`text-left ml-2`};
+
+  @media (max-width: 639px) {
+    ${tw`hidden`};
+  }
+`;
+
+export default NavigationButton
