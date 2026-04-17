410c29783ea35bc986d6f1c38c21ee4a32568044
Muhammad Zaki <162775830+jakisoft@users.noreply.github.com>
2026-04-17 13:22:15 +0700

    Upload ElysiumTheme/pterodactyl/resources/scripts/components/elements/elysium/announcement/AnnouncementContainer.tsx

---
+++ ElysiumTheme/pterodactyl/resources/scripts/components/elements/elysium/announcement/AnnouncementContainer.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components/macro';
+import tw from 'twin.macro';
+
+const AnnouncementContainer = styled.div`
+    ${tw`flex items-center rounded-2xl shadow px-4 py-3 m-4 text-white`}
+`;
+
+export default AnnouncementContainer;
