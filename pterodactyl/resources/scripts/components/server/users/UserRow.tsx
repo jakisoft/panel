12efe238815e48659e1b1e88aa47f83ef68c2c33
Muhammad Zaki <162775830+jakisoft@users.noreply.github.com>
2026-04-17 12:57:13 +0700

    Upload pterodactyl/resources/scripts/components/server/users/UserRow.tsx

---
+++ pterodactyl/resources/scripts/components/server/users/UserRow.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Subuser } from "@/state/server/subusers";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPencilAlt,
+  faUnlockAlt,
+  faUserLock,
+} from "@fortawesome/free-solid-svg-icons";
+import RemoveSubuserButton from "@/components/server/users/RemoveSubuserButton";
+import EditSubuserModal from "@/components/server/users/EditSubuserModal";
+import Can from "@/components/elements/Can";
+import { useStoreState } from "easy-peasy";
+import tw from "twin.macro";
+import GreyRowBox from "@/components/elements/GreyRowBox";
+import { Button } from "@/components/elements/button/index";
+
+interface Props {
+  subuser: Subuser;
+}
+
+export default ({ subuser }: Props) => {
+  const uuid = useStoreState((state) => state.user!.data!.uuid);
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <GreyRowBox css={tw`mb-2`}>
+      <EditSubuserModal
+        subuser={subuser}
+        visible={visible}
+        onModalDismissed={() => setVisible(false)}
+      />
+      <div
+        css={tw`w-10 h-10 rounded-full bg-white border-2 border-neutral-800 overflow-hidden hidden md:block`}
+      >
+        <img css={tw`w-full h-full`} src={`${subuser.image}?s=400`} />
+      </div>
+      <div css={tw`ml-4 flex-1 overflow-hidden`}>
+        <p css={tw`text-sm truncate`}>{subuser.email}</p>
+      </div>
+      <div css={tw`ml-4`}>
+        <p css={tw`font-medium text-center`}>
+          &nbsp;
+          <FontAwesomeIcon
+            icon={subuser.twoFactorEnabled ? faUserLock : faUnlockAlt}
+            fixedWidth
+            css={!subuser.twoFactorEnabled ? tw`text-red-400` : undefined}
+          />
+          &nbsp;
+        </p>
+        <p css={tw`text-2xs text-neutral-500 uppercase hidden md:block`}>
+          2FA Enabled
+        </p>
+      </div>
+      <div css={tw`mx-4 hidden md:block`}>
+        <p css={tw`font-medium text-center`}>
+          {
+            subuser.permissions.filter(
+              (permission) => permission !== "websocket.connect"
+            ).length
+          }
+        </p>
+        <p css={tw`text-2xs text-neutral-500 uppercase`}>Permissions</p>
+      </div>
+      {subuser.uuid !== uuid && (
+        <>
+          <Can action={"user.update"}>
+            <Button.Text
+              type={"button"}
+              css={tw`block w-8 h-8 transition-transform duration-100 mr-2`}
+              onClick={() => setVisible(true)}
+            >
+              <FontAwesomeIcon icon={faPencilAlt} />
+            </Button.Text>
+          </Can>
+          <Can action={"user.delete"}>
+            <RemoveSubuserButton subuser={subuser} />
+          </Can>
+        </>
+      )}
+    </GreyRowBox>
+  );
+};
