c43f4151e8bd7673ce42ae428ab30a29ba98fddb
Muhammad Zaki <162775830+jakisoft@users.noreply.github.com>
2026-04-17 13:22:17 +0700

    Upload ElysiumTheme/pterodactyl/resources/scripts/components/elements/elysium/navigation/CategoryContainer.tsx

---
+++ ElysiumTheme/pterodactyl/resources/scripts/components/elements/elysium/navigation/CategoryContainer.tsx
@@ -0,0 +1,13 @@
+import tw from "twin.macro";
+import styled from "styled-components/macro";
+
+const CategoryContainer = styled.div`
+  ${tw`w-full ml-4 mt-5 mb-1 uppercase font-semibold`};
+
+  & > a {
+    @media (max-width: 639px) {
+      ${tw`hidden`};
+  }
+`;
+
+export default CategoryContainer
