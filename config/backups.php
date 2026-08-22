b79de15bf640504392fa0ffa8cf394b3630fc75f
jakisoft <kiicodeofficial@gmail.com>
2026-08-22 15:36:10 +0000

    update backup system

--- config/backups.php
+++ config/backups.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'default' => env('APP_BACKUP_DRIVER', 'wings'),
+
     'default_provider' => env('APP_BACKUP_PROVIDER', 'local'),
 
     'r2' => [
@@ -18,5 +20,29 @@ return [
         'client_secret' => env('GOOGLE_DRIVE_CLIENT_SECRET', ''),
         'refresh_token' => env('GOOGLE_DRIVE_REFRESH_TOKEN', ''),
         'folder_id' => env('GOOGLE_DRIVE_FOLDER_ID', ''),
+    ],
+
+    'disks' => [
+        'wings' => [
+            'adapter' => 'wings',
+        ],
+        's3' => [
+            'adapter' => 's3',
+            'region' => env('AWS_DEFAULT_REGION', 'auto'),
+            'key' => env('AWS_ACCESS_KEY_ID', ''),
+            'secret' => env('AWS_SECRET_ACCESS_KEY', ''),
+            'bucket' => env('AWS_BACKUPS_BUCKET', ''),
+            'endpoint' => env('AWS_ENDPOINT', ''),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        ],
+        'r2' => [
+            'adapter' => 's3',
+            'region' => 'auto',
+            'key' => env('CLOUDFLARE_R2_ACCESS_KEY_ID', ''),
+            'secret' => env('CLOUDFLARE_R2_SECRET_ACCESS_KEY', ''),
+            'bucket' => env('CLOUDFLARE_R2_BUCKET', ''),
+            'endpoint' => env('CLOUDFLARE_R2_ENDPOINT', ''),
+            'use_path_style_endpoint' => false,
+        ],
     ],
 ];
